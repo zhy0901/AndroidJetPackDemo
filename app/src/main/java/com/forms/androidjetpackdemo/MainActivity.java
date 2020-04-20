3295fdd3d7bb1d6e745c1cf86245fc58b3c2f12e
zhouhongyang <zhouhongyang@formssi.com>
2020-04-20 20:49:48 +0800

    首次提交

---
+++ app/src/main/java/com/forms/androidjetpackdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.forms.androidjetpackdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.fragment).navigateUp();
+    }
+}
